beae5819f527ef8bd4a6441a560aae9105b5cf2f
Eva Pace <eba.pachi@gmail.com>
2024-08-03 12:20:25 -0300

    basic window

---
+++ main.lua
@@ -0,0 +1,34 @@
+local push = require('vendor.push')
+
+GAME_WIDTH = 432
+GAME_HEIGHT = 243
+
+function love.load()
+    love.window.setTitle('s n a k e')
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(GAME_WIDTH, GAME_HEIGHT, 1280, 720, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:start()
+
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    push:finish()
+end
